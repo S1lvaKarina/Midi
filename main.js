550fc54c0b107db1bafe08d87e9e9e780530c0c0
kakakakaka <108530728+S1lvaKarina@users.noreply.github.com>
2023-09-13 12:36:10 -0300

    s

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function tocaSomPom () {
 document.querySelector("#Som_Tecla_Pom").play();
 }
 
-document.querySelector("tecla_pom").onclick = tocaSomPom;
+document.querySelector("tecla_pom").onclick = tocaSomPom();
 
 
 
