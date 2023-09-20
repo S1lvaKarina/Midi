69d75757f8c862e24bde0d06ec255f80f45b4e07
kakakakaka <108530728+S1lvaKarina@users.noreply.github.com>
2023-09-20 12:25:44 -0300

    s

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
-
+//tecla Pom
 function tocaSomPom () {
 document.querySelector("#Som_Tecla_Pom").play();
 
 }
-
 document.querySelector("tecla_pom").onclick = tocaSomPom;
+
+//tecla clap
+function tocaSomClap () {
+    document.querySelector("#Som_Tecla_Clap").play();
+    
+    }
+    document.querySelector("tecla_clap").onclick = tocaSomClap;
+    
+//tecla tim
+function tocaSomTim () {
+    document.querySelector("#Som_Tecla_Tim").play();
+    
+    }
+    document.querySelector("tecla_tim").onclick = tocaSomTim;
+    
+//tecla puff
+function tocaSomPuff () {
+    document.querySelector("#Som_Tecla_Puff").play();
+    
+    }
+    document.querySelector("tecla_puff").onclick = tocaSomPuff;
+    
 
 
